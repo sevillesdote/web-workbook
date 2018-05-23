59c6146edbe45a6a11a10fa145b7153f6f0b6992
Seville Sdote <sevillesdote@gmail.com>
2018-05-23 16:14:59 -0500

    figured out negative function but it only works on first number before i put in an operator, not on second number

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -11,16 +11,29 @@ function addition() {
 }
 
 function subtraction() {
-  document.querySelector("#results").value -= "-";
+  document.querySelector("#results").value += "-";
 }
 
 function multiply() {
-  document.querySelector("#results").value *= "*";
+  document.querySelector("#results").value += "*";
 }
 
 function division() {
-  document.querySelector("#results").value /= "/";
+  document.querySelector("#results").value += "/";
 }
+
+function negative() {
+  document.querySelector("#results").value = -Math.abs(document.querySelector("#results").value);
+}
+
+// function negative() {
+//   let current = document.querySelector("#results").value;
+//   document.querySelector("#results").value = -Math.abs(current.slice(0, 1));
+// }
+
+// function negative() {
+//   "-" += document.querySelector("#results").value;
+// }
 
 function equals() {
   document.querySelector("#results").value = eval(document.querySelector("#results").value);
@@ -30,24 +43,3 @@ function deleteLast() {
   let current = document.querySelector("#results").value;
   document.querySelector("#results").value = current.slice(0, -1);
 }
-
-// function addNumber(num) {
-//   document.querySelector("#results").value += num;
-// }
-//
-// function clearResults() {
-//   document.querySelector("#results").value = "";
-// }
-//
-// function addition() {
-//   document.querySelector("#results").value += "+";
-// }
-//
-// function equals() {
-//   document.querySelector("#results").value = eval(document.querySelector("#results").value);
-// }
-//
-// function deleteLast() {
-//   let current = document.querySelector("#results").value;
-//   document.querySelector("#results").value = current.slice(0, -1);
-// }
